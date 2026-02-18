548288718b31e944aae5a0b655f2490252b7ed5f
Meiirzhan Baitangatov <rikimarubro@gmail.com>
2026-02-18 21:18:07 +0500

    Solution-1672

---
+++ solution-1672.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+const maximumWealth = function (accounts) {
+    let rich = 0;
+    for (const i of accounts) {
+        let sum = 0;
+        for (const money of i) {
+            sum += money;
+        }
+        rich = Math.max(rich, sum);
+    }
+    return rich;
+};
+
+// Test
+console.log(
+    maximumWealth([
+        [3, 1, 2, 10, 1],
+        [3, 5, 4, 6, 8],
+    ]),
+); // Expected: [0, 1]
