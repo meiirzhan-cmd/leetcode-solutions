05f0d33b0389809dc932cae5db6e7f756f9df1d7
Meiirzhan Baitangatov <rikimarubro@gmail.com>
2026-02-19 19:46:22 +0500

    Solution 1342

---
+++ solution-1342.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+const numberOfSteps = function (num) {
+    let steps = 0;
+    while (num !== 0) {
+        steps++;
+        num = num % 2 === 0 ? num / 2 : num - 1;
+    }
+    return steps;
+};
+
+console.log(numberOfSteps(9));
